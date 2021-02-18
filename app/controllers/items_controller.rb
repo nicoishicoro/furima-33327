8717747bd5c80d4b5e9c9ac680b03072666f837e
nicoishicoro <kakutani_haruka@yahoo.co.jp>
2021-02-18 19:33:12 +0900

    ルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,13 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @items = Item.new
+  end
+
+  def create
+  end
+
   def items_params
     params.require(:items).permit(:name, :image).merge(user_id: current_user.id)
   end
