60a5655ae6d296319647da3756d3263b9d8df73c
nicoishicoro <kakutani_haruka@yahoo.co.jp>
2021-02-18 11:09:22 +0900

    ユーザー新規登録のテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
+  
+  validates :password,        presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字を含めてください'}
+  
+  with_options presence: true, format: { with: /\A[ぁ-ん一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナ文字を使用してください' } do
   validates :last_name_kana,  presence: true
   validates :first_name_kana, presence: true
+  end
+
   validates :birth_date,      presence: true
 
   has_many :items
   has_many :purchase_items
+
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"aaa"}
+    email                 {"aaa@aaa"}
+    password              {"aaa000"}
+    password_confirmation {"aaa000"}
+    last_name             {"ああああ"}
+    first_name            {"いいいい"}
+    last_name_kana        {"アアアア"}
+    first_name_kana       {"イイイイ"}
+    birth_date            {"2000/2/2"}
+  end
+end
