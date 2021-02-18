968f7c692ee6edc2763132bc6fff35d047a9ac91
nicoishicoro <kakutani_haruka@yahoo.co.jp>
2021-02-18 14:40:20 +0900

    エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-
   validates :password,        presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字を含めてください' }
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
 
   with_options presence: true, format: { with: /\A[ぁ-ん一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
@@ -17,8 +20,6 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :first_name_kana
   end
-
-  validates :birth_date, presence: true
 
   has_many :items
   has_many :purchase_items
