19d24d8c4f1c4a7971cf1dea111f9c001b6eb244
nicoishicoro <kakutani_haruka@yahoo.co.jp>
2021-04-02 11:59:35 +0900

    修正３

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe OrderDestination, type: :model do
     before do
-      @order_destination = FactoryBot.build(:order_destination, user_id: 1, item_id: 1)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_destination = FactoryBot.build(:order_destination, user_id: user.id , item_id: item.id )
+      sleep 0.1
     end
 
     describe '商品購入' do
